621080d0286b44282c3cc6af49f0667c0338a1d3
keidy001 <ksidy006@outlook.com>
2025-11-08 11:36:08 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   // base: '',
-  base: '/portfolio/',
+  // base: '/portfolio/',
   plugins: [
     react(),
     mode === 'development' &&
